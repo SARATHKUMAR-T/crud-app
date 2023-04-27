f1ac247b39d509e89059229bc6ed2ae0ea20a513
SARATHKUMAR-T <sarathkumartk98@gmail.com>
2023-04-27 11:13:02 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Switch,useState } from "react";
+import { Switch, useState } from "react";
 import { BrowserRouter as Route } from "react-router-dom";
 import "./App.css";
 import data from "./data/data";
@@ -11,18 +11,11 @@ function App() {
   const [students, setStudents] = useState(data);
   return (
     <div className="App min-h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
-      <Switch>
-
-        <Route exact path="/">
-          <Student
-          Students={students}
-          setStudents={setStudents}/>    
-        </Route>
-
-
-       
-     
-      </Switch>
+      <Route exact path="/">
+        <Switch>
+          <Student Students={students} setStudents={setStudents} />
+        </Switch>
+      </Route>
     </div>
   );
 }
